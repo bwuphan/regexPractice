1b0b36a1986f64a8a5fd0b0c77e39f7a188d3397
Bobby <bwuphan@gmail.com>
2018-01-11 21:52:02 -0800

    Update email regex to capture only domain

--- email.js
+++ email.js
@@ -1,10 +1,10 @@
 const getDomain = (email) => {
-  const regex = /[A-Za-z0-9.!#$%&'*+-/=?^_`{|}]+@(.+)/g;
+  const regex = /[A-Za-z0-9.!#$%&'*+-/=?^_`{|}]+@(.+)\.\w+/g;
   return regex.exec(email)[1];
 }
 
 
-const testArr = ['bwuphan@gmail.com', 't@ucdavis.edu', 'df.er@yahoo'];
+const testArr = ['bwuphan@gmail.com', 't@ucdavis.edu', 'df.er@yahoo.com', 'snlam@u.o.pacific.edu'];
 
 for (let i = 0; i < testArr.length; i++) {
   console.log(getDomain(testArr[i]));
