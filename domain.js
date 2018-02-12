d312830c44f401393896d1fb100d1ad6cee30f58
Bobby <bwuphan@gmail.com>
2018-02-11 21:45:03 -0800

    Add case for domain.js

--- domain.js
+++ domain.js
@@ -1,8 +1,8 @@
-const getDomain = () => {
+const getDomain = (url) => {
 
 }
 
-const testArr = ['google.com', 'www.yahoo.com', 'http://www.espn.com', 'https://www.pacific.edu'];
+const testArr = ['google.com', 'www.yahoo.com', 'http://www.espn.com', 'https://www.pacific.edu', 'www.r1.com'];
 
 for (let i = 0; i < testArr.length; i++) {
   console.log(getDomain(testArr[i]));
